b2b21b92d6db35496e9549eaf636846539a44045
c581756b61737a20506965637a6b65 <L.Pieczke@proton.me>
2025-09-07 19:55:17 +0200

    Added labels disko.nix

--- disko.nix
+++ disko.nix
@@ -1,43 +1,37 @@
-# disko.nix
-{ pkgs, lib, ... }:
-
-{
-  disko.enableConfig = true;
-
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          esp = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
+disko.devices = {
+  disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        esp = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
           };
+        };
 
-          btrfs = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "btrfs";
-              mountpoint = null;
-              subvolumes = {
-                root = { mountpoint = "/"; };
-                persist = { mountpoint = "/persist"; };
-                nix = {
-                  mountpoint = "/nix";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
-                home = {
-                  mountpoint = "/home";
-                  mountOptions = [ "compress=zstd" "noatime" ];
-                };
+        btrfs = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "btrfs";
+            label = "btrfs";  # <- add label here
+            mountpoint = null;
+            subvolumes = {
+              root = { mountpoint = "/"; };
+              persist = { mountpoint = "/persist"; };
+              nix = {
+                mountpoint = "/nix";
+                mountOptions = [ "compress=zstd" "noatime" ];
+              };
+              home = {
+                mountpoint = "/home";
+                mountOptions = [ "compress=zstd" "noatime" ];
               };
             };
           };
@@ -45,5 +39,5 @@
       };
     };
   };
-}
+};
 
