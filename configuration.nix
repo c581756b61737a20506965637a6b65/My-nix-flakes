49d8d1d8e8a6af3012d56d9cd94a31954009eeb4
c581756b61737a20506965637a6b65 <L.Pieczke@proton.me>
2025-09-07 19:52:43 +0200

    Replaced configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,49 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
-}:
+# configuration.nix
+{ config, pkgs, lib, specialArgs, ... }:
 
 {
   imports = [ ];
 
   system.stateVersion = "23.11";
 
+  # Bootloader: systemd-boot for EFI
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Swap file for hibernation
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024; # 16 GiB
+    }
+  ];
+
+  # File systems mounting Btrfs subvolumes created by Disko
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/btrfs";
+    fsType = "btrfs";
+    options = [ "subvol=root" "compress=zstd" "noatime" ];
+  };
+
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-label/btrfs";
+    fsType = "btrfs";
+    options = [ "subvol=persist" "compress=zstd" "noatime" ];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-label/btrfs";
+    fsType = "btrfs";
+    options = [ "subvol=nix" "compress=zstd" ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/btrfs";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress=zstd" ];
+  };
+
+  # Impermanence persistence configuration
   environment.persistence."/persist" = {
     neededForBoot = true;
     directories = [ "/etc/nixos" ];
@@ -20,36 +53,22 @@
   environment.persistence."/home" = {
     neededForBoot = true;
     users = {
-      "${username}" = {
-        directories = [
-          "Documents"
-          "Games"
-          "Programming"
-          "Study"
-        ];
-        files = [ ];
+      "${specialArgs.username}" = {
+        directories = [ "Documents" "Games" "Programming" "Study" ];
       };
     };
   };
 
-  home-manager.users."${username}".useUserPackages = true;
-  home-manager.users."${username}".configuration = import ./Home/stylix.nix;
-
-  environment.systemPackages = with pkgs; [ ];
-
-  services.xserver.enable = false;
-
-  # Some hibernate swap stuff
-  # Enable resume support
+  # Sleep & hibernation kernel and systemd settings
   boot.initrd.kernelModules = [ "resume" ];
+  boot.kernelParams = [ "resume=/var/lib/swapfile" ];
 
-  # Point to swap device
-  boot.kernelParams = [ "resume=/dev/nvme0n1p3" ]; # swap partition
-
-  # Recommended systemd sleep settings
   systemd.sleep.extraConfig = ''
     AllowSuspend=yes
     AllowHibernation=yes
     AllowSuspendThenHibernate=yes
   '';
+
+  # Disable X server 
+  services.xserver.enable = false;
 }
